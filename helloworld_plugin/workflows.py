5037849634ecbdce62f280c6d02b634e52bb58cb
Konrad Paziewski <konrad.paziewski@codilime.com>
2015-07-14 11:11:04 +0000

    fix

--- helloworld_plugin/workflows.py
+++ helloworld_plugin/workflows.py
@@ -1,12 +1,17 @@
 from cloudify.decorators import workflow
 from cloudify.workflows import ctx
 
+import logging
+
 @workflow
 def hello(**kwargs):
-    graph = ctx.graph_mode()
+    log = logging.getLogger(__name__)
+    log.warn('in hello')
+       
+    #graph = ctx.graph_mode()
 
-    for node in ctx.nodes:
-        for instance in node.instances:
-            graph.add_task(instance.execute_operation('cloudify.interfaces.lifecycle.configure'))
+    #for node in ctx.nodes:
+    #    for instance in node.instances:
+    #        graph.add_task(instance.execute_operation('cloudify.interfaces.lifecycle.configure'))
 
-    return graph.execute()
+    #return graph.execute()
