5e1d030224cc013dbc3b287e6014f65e4a60573a
Konrad Paziewski <konrad.paziewski@codilime.com>
2015-07-14 11:22:06 +0000

    rand

--- helloworld_plugin/workflows.py
+++ helloworld_plugin/workflows.py
@@ -8,10 +8,10 @@ def hello(**kwargs):
     log = logging.getLogger(__name__)
     log.warn('in hello')
        
-    #graph = ctx.graph_mode()
+    graph = ctx.graph_mode()
 
-    #for node in ctx.nodes:
-    #    for instance in node.instances:
-    #        graph.add_task(instance.execute_operation('cloudify.interfaces.lifecycle.configure'))
+    for node in ctx.nodes:
+        for instance in node.instances:
+            graph.add_task(instance.execute_operation('helloworld_plugin.tasks.operation_log'))
 
-    #return graph.execute()
+    return graph.execute()
